ee103cb790f90106f6b04b7ffe6fce82549e3e9e
JEFFGULICK <jeffgulick@gmail.com>
2020-10-11 23:15:57 -0500

    google cloud instance

--- sql/connection.js
+++ sql/connection.js
@@ -7,9 +7,9 @@ class Connection {
 
       const config = {
         connectionLimit: 100,
-        host: 'localhost',
+        host: '34.121.0.103',
         user: 'root',
-        password: 'password',
+        password: 'hello',
         database: 'admin'
       }
 
